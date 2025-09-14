4726bf4a6d42c5737c23f25c6996e321ae95a075
prakhyat798 <prakhyatmaurya58@gmail.com>
2025-09-15 00:01:24 +0530

    Delete fourth.py

--- fourth.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-
-for i in range(1, n+1):
-    for j in range(i):
-        if j % 2 == 0:
-            print(j+1, end=" ")
-        else:
-            print("*", end=" ")
-    print()
