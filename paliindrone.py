49f4d3d8fbbc5edc5f324efb8d0fa5384af407ad
prakhyat798 <prakhyatmaurya58@gmail.com>
2025-11-14 08:49:07 +0530

    Delete paliindrone.py

--- paliindrone.py
+++
@@ -1,8 +0,0 @@
-def is_palindrome(s):
-    if len(s) <= 1:
-        return True
-    if s[0] != s[-1]:
-        return False
-    return is_palindrome(s[1:-1])
-word = "madam"
-print(is_palindrome(word))
