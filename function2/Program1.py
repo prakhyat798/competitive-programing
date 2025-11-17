2d2d49aea04be340f811f8001949d3dbcc9b7c84
prakhyat798 <prakhyatmaurya58@gmail.com>
2025-11-17 10:41:29 +0530

    Add files via upload

---
+++ function2/Program1.py
@@ -0,0 +1,6 @@
+def introduce(name, age, profession):
+    print("Name:", name)
+    print("Age:", age)
+    print("Profession:", profession)
+
+introduce(name="Suyash", age=20, profession="Teacher")
